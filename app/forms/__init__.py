f2d104eb61ad24429863282edd447593cbed6dfd
thalath <tunthalath@gmail.com>
2026-02-11 21:59:23 +0700

    forms.__init__.py

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,16 +1,15 @@
 from app.forms.user_forms import UserCreateForm, UserEditForm, ConfirmDeleteForm
 from app.forms.role_forms import RoleCreateForm, RoleEditForm, RoleConfirmDeleteForm
 from app.forms.permission_forms import PermissionCreateForm, PermissionEditForm, PermissionConfirmDeleteForm
-
+from app.forms.category_forms import CategoryCreateForm, CategoryEditForm, CategoryConfirmDelete
+from app.forms.budget_forms import BudgetCreateForm, BudgetEditForm, BudgetConfirmDeleteForm
+from app.forms.expense_forms import ExpenseCreateForm, ExpenseEditForm, ExpenseConfirmDelete
 
 __all__ = [
-    "UserCreateForm",
-    "UserEditForm",
-    "ConfirmDeleteForm",
-    "RoleCreateForm",
-    "RoleEditForm",
-    "RoleConfirmDeleteForm",
-    "PermissionCreateForm",
-    "PermissionEditForm",
-    "PermissionConfirmDeleteForm",
+    "UserCreateForm", "UserEditForm", "ConfirmDeleteForm",
+    "RoleCreateForm", "RoleEditForm", "RoleConfirmDeleteForm",
+    "PermissionCreateForm", "PermissionEditForm", "PermissionConfirmDeleteForm",
+    "CategoryCreateForm", "CategoryEditForm", "CategoryConfirmDelete",
+    "BudgetCreateForm", "BudgetEditForm", "BudgetConfirmDeleteForm",
+    "ExpenseCreateForm", "ExpenseEditForm", "ExpenseConfirmDelete"
 ]
