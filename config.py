a08eb0a44aba24e408a8a5bdc050b4858853646f
Thalath Tun <tunthalath@gmail.com>
2026-03-30 17:46:43 +0700

    adding table style

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")
-    SQLALCHEMY_DATABASE_URI = (os.environ.get("DATABASE_URL")
-        or "sqlite:///" + os.path.join(BASE_DIR, "instance", "expense_tracking_system.db"))
-
+    
+    url = os.environ.get("DATABASE_URL")
+    data_file = "sqlite:///" + os.path.join(BASE_DIR, "instance", "expense_tracking_system.db")
+    
+    SQLALCHEMY_DATABASE_URI = (url or data_file)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
